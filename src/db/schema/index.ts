c238a0717f0756f8d5545dec2a7239da343e3614
Henrique Biruel Furtado <202210347@al.facamp.com.br>
2025-07-08 20:27:29 -0300

    feat: add create question, create room and get room questions

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,5 +1,10 @@
+import { questions } from './questions.ts';
 import { rooms } from './rooms.ts';
 
 export const schema = {
+  // Define your schema here
+  // For example, you can import and use the rooms table
   rooms,
+  questions
+
 };
